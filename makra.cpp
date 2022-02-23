c9af9949b5adb902d5aa16baccc588da89ee0616
G00DCOD3R <82707639+G00DCOD3R@users.noreply.github.com>
2022-02-23 19:55:47 +0100

    first commit

---
+++ makra.cpp
@@ -0,0 +1,31 @@
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second 
+#define all(...) begin(__VA_ARGS__) , end(__VA_ARGS__)
+#define boost {ios_base::sync_with_stdio(false); cin.tie(); cout.tie();}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define random_shuffle(...) shuffle(__VA_ARGS__, rng)
+#define random(...) uniform_int_distribution<int>(__VA_ARGS__)(rng)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+constexpr ll INF = 2e18+6969;
+
+
+
+int main()
+{
+	
+	
+	return 0;
+}
